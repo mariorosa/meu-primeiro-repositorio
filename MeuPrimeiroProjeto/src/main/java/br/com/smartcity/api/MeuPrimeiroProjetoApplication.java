c2032d010bf46d060e62cae88fe9731e510cd10f
MarioRosa <mario.mrs@gmail.com>
2017-12-25 12:51:01 -0200

    Alteracao no arquivo principal da aplicacao

--- MeuPrimeiroProjeto/src/main/java/br/com/smartcity/api/MeuPrimeiroProjetoApplication.java
+++ MeuPrimeiroProjeto/src/main/java/br/com/smartcity/api/MeuPrimeiroProjetoApplication.java
@@ -8,5 +8,9 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("primeiro teste");
+		
+		
+		
 	}
 }
